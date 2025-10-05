341292c8788a2d6df1fcda7e3f9a1b0cfb2b5fe7
daniel sodre <dsodre2003@gmail.com>
2025-10-05 12:39:48 -0300

    feat: criar componente header global

---
+++ src/app/componentes/header/header.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent{}
