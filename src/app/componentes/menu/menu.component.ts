21352e7e568c9d1aa03c67ba4bca642fae6ed276
daniel sodre <dsodre2003@gmail.com>
2025-10-23 19:39:08 -0300

    feat(menu): adiciona menu principal, submenu de admin e validação de acesso de administradores

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { IonList, IonItem, IonHeader, IonToolbar, 
-  IonTitle, IonMenuToggle, IonAccordionGroup, IonAccordion, IonIcon} from '@ionic/angular/standalone';
-import { RouterLinkWithHref } from '@angular/router';
+  import { Component } from '@angular/core';
+  import { IonList, IonItem, IonHeader, IonToolbar,
+    IonTitle, IonMenuToggle, IonAccordionGroup, IonAccordion} from '@ionic/angular/standalone';
+  import { RouterLinkWithHref } from '@angular/router';
+  import { AuthService } from 'src/app/services/autenticador/auth.service';
+  import { CommonModule } from '@angular/common';
+  import { Observable, take } from 'rxjs';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
-  standalone: true,
-  imports: [IonList, IonItem, IonHeader,
-     IonToolbar, IonTitle, IonMenuToggle, 
-     RouterLinkWithHref, IonAccordionGroup, 
-     IonAccordion] ,
-})
-export class MenuComponent {}
+  @Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+    standalone: true,
+    imports: [IonList, IonItem, IonHeader,
+      IonToolbar, IonTitle, IonMenuToggle,
+      RouterLinkWithHref, IonAccordionGroup,
+      IonAccordion, CommonModule],
+  })
+  export class MenuComponent {
+      public readonly isAdmin$: Observable<boolean>;
+
+    constructor(private auth: AuthService){
+       this.isAdmin$ = this.auth.isAdmin$;
+    }
+
+    logout(){ this.auth.logout(); }
+  }
---
+++ src/app/guards/admin/admin.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from 'src/app/services/autenticador/auth.service';
+
+export const AdminGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (!auth.isAuthenticated()) {
+    router.navigate(['/login']); // não logado
+    return false;
+  }
+
+  if (auth.isAdmin$) {
+    return true; // logado e admin
+  }
+
+  router.navigate(['/home']); // logado, mas não admin
+  return false;
+};
