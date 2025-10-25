c8f6ec5121cfd487f1d74827f292ccb9e7e02a9d
daniel sodre <dsodre2003@gmail.com>
2025-10-25 20:13:01 -0300

    feat(auth): Implementar busca da role do usuário no Firebase

--- src/app/services/autenticador/auth.service.ts
+++ src/app/services/autenticador/auth.service.ts
@@ -4,6 +4,7 @@ import { UserRole } from 'src/app/interfaces/user-role.enum';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Auth, signInWithEmailAndPassword, signOut, User } from '@angular/fire/auth';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,37 @@ export class AuthService {
   private role$ = new BehaviorSubject<UserRole | null>(null);
   isAdmin$ = this.role$.asObservable().pipe(map(r => r === UserRole.Admin));
 
-  constructor(private router: Router, private auth: Auth) {}
+  constructor(private router: Router, private auth: Auth,
+    private firestore: Firestore) { }
 
   // Login usando Firebase
   async login(email: string, password: string): Promise<boolean> {
     try {
+      // Faz login no Firebase Authentication com e-mail e senha
       const cred = await signInWithEmailAndPassword(this.auth, email, password);
 
-      // Exemplo: definir role baseado no email
-      if (cred.user.email === 'admin@exemplo.com') {
-        this.setRole(UserRole.Admin);
-      } else {
-        this.setRole(UserRole.User);
-      }
+      // Pega o UID (identificador único) do usuário autenticado
+      const uid = cred.user.uid;
 
+      // Cria uma referência para o documento do usuário no Firestore (coleção "usuarios")
+      const docRef = doc(this.firestore, 'usuarios', uid);
+
+      // Busca os dados do documento correspondente ao UID
+      const snap = await getDoc(docRef);
+
+      // Se o documento existir no banco...
+      if (snap.exists()) {
+        // Obtém os dados do usuário (ex: nome, email, role, etc.)
+        const data = snap.data() as any;
+
+        // Extrai a role (função do usuário, ex: Admin, User)
+        const role = data?.role as UserRole;
+
+        // Armazena a role no BehaviorSubject interno para uso posterior
+        this.setRole(role);
+      }else {
+        this.setRole(null);
+      }
       return true;
     } catch (err) {
       console.error('Erro no login:', err);
